f63e1d964dccdadfc33a57fc382f38cfd4ddf66e
Trauvin <mateusgalote@gmail.com>
2021-12-22 20:43:10 -0300

    add

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import Clock from "./modules/clock.js";
 
 // O future não pode compratilhar do objeto now
 // pois a diferença seria sempre a mesma nesse caso
+
 const now = new Date();
 const now25 = now.setMinutes(now.getMinutes() + 50);
 const clock = new Clock(now25);
