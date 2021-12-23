756089db5c67e1c90b90594e5a44939a05c26c19
Trauvin <mateusgalote@gmail.com>
2021-12-22 21:33:32 -0300

    adicionada funcao para iniciar a contagem

--- src/script.js
+++ src/script.js
@@ -3,10 +3,31 @@ import Clock from "./modules/clock.js";
 // O future não pode compratilhar do objeto now
 // pois a diferença seria sempre a mesma nesse caso
 
-const now = new Date();
-const now25 = now.setMinutes(now.getMinutes() + 50);
-const clock = new Clock(now25);
+function startTimer() {
+  let now = new Date();
+  const minutes = document.querySelector('.minutes');
+  const seconds = document.querySelector('.seconds');
+  const btns = document.querySelectorAll('.timers')
 
-setInterval(() => {
-  console.log(clock.total())
-}, 1000)
+  btns.forEach(btn => {
+
+    let timer = +btn.innerText;
+
+    function setTime() {
+      const newTime = now.setMinutes(now.getMinutes() + timer);
+      const clock = new Clock(newTime);
+
+      setInterval(() => {
+        minutes.innerText = clock.total().minutes;
+        seconds.innerText = ' : ' + clock.total().seconds;
+      }, 1000);
+    }
+
+    btn.addEventListener('click', setTime);
+  });
+}
+
+startTimer();
+
+
+
